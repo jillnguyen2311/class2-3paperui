8f3e582524b9d054c4f3a52f1b4e12186408c59b
Hoai Nguyen <hnguyen238@se14l0121-07.ad.bcit.ca>
2023-09-12 16:37:31 -0700

    init

---
+++ screens/Home.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { Avatar, Button, Card, Text } from 'react-native-paper';
+
+export default function Home({ navigation }) {
+    const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <Text>Welcome!</Text>
+                <StatusBar style="auto" />
+                <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+                    Press me
+                </Button>
+
+                <Card>
+                    <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+                    <Card.Content>
+                        <Text variant="titleLarge">Card title</Text>
+                        <Text variant="bodyMedium">Card content</Text>
+                    </Card.Content>
+                    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+                    <Card.Actions>
+                        <Button>Cancel</Button>
+                        <Button>Ok</Button>
+                    </Card.Actions>
+                </Card>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
